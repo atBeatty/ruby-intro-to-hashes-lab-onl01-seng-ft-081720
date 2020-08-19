10dcc0d6027d7c32be0f61c06333af01164c5438
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-19 17:41:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
 			:pieces => 4,
 			:names => {
 				:reading_railroad => {},
-				:pennsylvania_railroad => {}
+				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
 			},
